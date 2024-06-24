3633d0c8eb52bf38aa5cf6ca18d00e4288b4c80e
Akshay Vilas Jadhav <52353967+Akshay0701@users.noreply.github.com>
2024-06-24 14:55:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -133,8 +133,8 @@ def get_images():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=8000)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
 
 
 
