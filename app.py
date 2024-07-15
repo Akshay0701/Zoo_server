724528c754a90e26acee4d8fce2a45ad0b691cd6
Akshay Vilas Jadhav <52353967+Akshay0701@users.noreply.github.com>
2024-07-15 16:00:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def process_image():
     
     # process = subprocess.Popen(script_command)
     # process.wait()
+    time.sleep(32)
     
     # Return the job ID immediately
     return redirect(url_for('show_image', user_folder="20424029"))
