59a2bff797d50be66b73adc7dbba1d066969ac54
Akshay Vilas Jadhav <52353967+Akshay0701@users.noreply.github.com>
2024-07-15 16:10:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,33 +10,9 @@ import os
 import pandas as pd
 import logging
 import time
-from celery import Celery
 
 app = Flask(__name__)
 app.config['TIMEOUT'] = 90  # Example: 90 seconds
-app.config.update(
-    CELERY_BROKER_URL='redis://localhost:6379/0',  # Redis broker URL
-    CELERY_RESULT_BACKEND='redis://localhost:6379/0'  # Redis backend URL
-)
-
-def make_celery(app):
-    celery = Celery(
-        app.import_name,
-        backend=app.config['CELERY_RESULT_BACKEND'],
-        broker=app.config['CELERY_BROKER_URL']
-    )
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-
-    class ContextTask(TaskBase):
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-
-    celery.Task = ContextTask
-    return celery
-
-celery = make_celery(app)
 
 @app.route('/')
 def index():
@@ -207,7 +183,6 @@ def get_images():
 if __name__ == '__main__':
     # app.run(debug=True)
     app.run(host='0.0.0.0', port=8000, debug=True)
-    celery.start()
 
 
 
